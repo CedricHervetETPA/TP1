be24ec3cea42aa9d4bfb9ef3c27c65f3e376776b
CedricHervetETPA <cedric.hervet49@gmail.com>
2020-01-13 18:00:22 +0100

    Update monJeu.c

--- monJeu.c
+++ monJeu.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int main() {
 int (pvMonstre); 
+int (pvJoueur);
+
 pvMonstre = 100;
+pvJoueur = 100;
 
 while (pvMonstre>0) {
 	printf("Vous frappez le Grand Chien-Loup et lui infligez 5 points de degats.\n");
 	pvMonstre = pvMonstre-5;
-	printf("Le Grand Chien-Loup est touché, il possede desormais %d points de vie.",pvMonstre);
+	printf("Le Grand Chien-Loup est atteint, il possede desormais %d points de vie.",pvMonstre);
+
+	printf("Le Grand Chien-Loup contre-attaque, il enlève au joueur %d points de vie.\n",pvJoueur);
+	pvJoueur = pvJoueur-5;
+	printf("Vous possedez maintenant %d points de vie.\n",pvJoueur);
 }
 }
+
 
