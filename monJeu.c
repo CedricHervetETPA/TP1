c9cb8f69b9b21b4f3a44a80493c234f472b678aa
CedricHervetETPA <cedric.hervet49@gmail.com>
2020-01-13 17:40:44 +0100

    Update monJeu.c
    Le personnage frappe le monstre jusqu'à ce que celui-ci tombe à 0 pv

--- monJeu.c
+++ monJeu.c
@@ -3,8 +3,10 @@ int main() {
 int (pvMonstre); 
 pvMonstre = 100;
 
-printf("Vous frappez le Grand Chien-Loup et lui infligez 5 points de degats.\n");
-pvMonstre = pvMonstre-5;
-
-printf("Le Grand Chien-Loup est touché, il possède désormais %d points de vie.",pvMonstre);
+while (pvMonstre>0) {
+	printf("Vous frappez le Grand Chien-Loup et lui infligez 5 points de degats.\n");
+	pvMonstre = pvMonstre-5;
+	printf("Le Grand Chien-Loup est touché, il possede desormais %d points de vie.",pvMonstre);
 }
+}
+
