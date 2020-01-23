aafc0c5ea9371db86619443795a62bdc00bd4a04
AlcideBaudouinETPA <59608459+AlcideBaudouinETPA@users.noreply.github.com>
2020-01-24 00:39:40 +0100

    Update monJeu.c

--- monJeu.c
+++ monJeu.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+ 
 
 int main(){
-	int pvMonstre; 
-	int pvJoueur;
+	int pvMonstre = 500; 
+	int pvJoueur = 500;
 	int choixJoueur;
-	int degatsJoueur;
-	int degatsMonstre;
-	int pointMagie;
-	int degatsPoison;
+	int degatsJoueur = 100;
+	int degatsMonstre = 20;
+	int pointMagie = 0;
+	int degatsPoison = 30;
+	int choixMonstre(void){
+	srand(time(NULL));
+    int nbgen=rand()%2+1;    //entre 1&2
+    int degatsPoisonMonstre = 50;
+    int compteurPoison = 0;
 
-
-	pvMonstre = 500;
-	pvJoueur = 1000;
-	degatsJoueur = 100;
-	degatsMonstre = 20;
-	pointMagie = 0;
-	degatsPoison = 30;
 
 
 /*combat*/
@@ -35,26 +36,36 @@ printf("Vous possedez %d points de vie.\n",pvJoueur);
 			printf("Vous frappez le Grand Chien-Loup et lui infligez %d points de degats.\n",degatsJoueur);
 			pvMonstre = pvMonstre-degatsJoueur;
 			printf("Le Grand Chien-Loup possede desormais %d points de vie.\n",pvMonstre);
-			printf("Le Grand Chien-Loup contre-attaque, il enleve au joueur %d points de vie.\n",degatsMonstre);
-			pvJoueur = pvJoueur-degatsMonstre;
-			printf("Vous possedez maintenant %d points de vie.\n",pvJoueur);
 		}
 		else if (choixJoueur==2) {
 			printf("Le Grand Chien-Loup attaque mais vous vous defendez, il vous enleve quand meme 5 points de vie.\n");
 			pvJoueur = pvJoueur-degatsMonstre/4;
 			printf("Le Grand Chien-Loup possede desormais %d points de vie.\n",pvMonstre);
-			printf("Vous possedez maintenant %d points de vie.\n",pvJoueur);
+
 
 		}
 		else if (choixJoueur==3) {
 			printf("vous infliger un poison au Grand Chien-Loup il ne pourra plus sans defaire avant la fin de la partie, ce poison lui infligera %d de degats a chaque tour.\n",degatsPoison );
 			pvMonstre = pvMonstre-degatsPoison
 			printf("Le Grand Chien-Loup possede desormais %d points de vie.\n",pvMonstre);
-			printf("Le Grand Chien-Loup contre-attaque, il enleve au joueur %d points de vie.\n",degatsMonstre);
-			pvJoueur = pvJoueur-degatsMonstre;
-			printf("Vous possedez maintenant %d points de vie.\n",pvJoueur);
+		
 		}
-
+		printf("(1) il attaque (2) il t'empoisonne\n");
+		if (nbgen==1){
+			printf("Le Grand Chien-Loup vous attaque, il vous enleve %d points de vie.\n",degatsMonstre);
+		}
+			pvJoueur = pvJoueur-degatsMonstre;
+		printf("Vous possedez maintenant %d points de vie.\n",pvJoueur);
+		
+			else if (nbgen==2){
+				for (compteurPoison<3){
+					compteurPoison++
+			printf("Le Grand Chien-Loup vous empoisonne, il vous enleve %d points de vie.\n",degatsMonstre);
+			printf("vous garderez ce poison en vous pendant 3 tours.  \n");
+				}	
+			}
+			pvJoueur = pvJoueur-degatsPoisonMonstre;
+			printf("Vous possedez maintenant %d points de vie.\n",pvJoueur);
 
 
 		/*fin de partie*/
